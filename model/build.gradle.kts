4b42c4417361937b9e11d5de95cd21ec75961e90
Antonio Corrales <desarrollogit@gmail.com>
2024-03-25 17:42:16 +0100

    - Change java version to 17

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -9,16 +9,16 @@ android {
     compileSdk = (findProperty("compileSdkVersion") as String).toInt()
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion = JavaLanguageVersion.of(17)
     }
 }
