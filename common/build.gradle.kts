688cfdd30d11daca75ad9931897c8065d43be1a3
Antonio Corrales <desarrollogit@gmail.com>
2024-03-26 08:23:41 +0100

    - Add minSdkVersion to common and model modules to avoid inject permissions

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -8,6 +8,11 @@ android {
     namespace = "com.desarrollodroide.common"
     compileSdk = (findProperty("compileSdkVersion") as String).toInt()
 
+    defaultConfig {
+        minSdk = (findProperty("minSdkVersion") as String).toInt()
+        targetSdk = (findProperty("targetSdkVersion") as String).toInt()
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -8,6 +8,11 @@ android {
     namespace = "com.desarrollodroide.model"
     compileSdk = (findProperty("compileSdkVersion") as String).toInt()
 
+    defaultConfig {
+        minSdk = (findProperty("minSdkVersion") as String).toInt()
+        targetSdk = (findProperty("targetSdkVersion") as String).toInt()
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
