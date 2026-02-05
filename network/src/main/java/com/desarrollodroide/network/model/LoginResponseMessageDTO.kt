19be68415394de5caa11c23076ae13ce526d6c3f
appel <61596+appel@users.noreply.github.com>
2026-02-05 11:48:40 -0500

    Refactor LoginResponseMessageDTO structure
    Refactor LoginResponseMessageDTO to map token to session.

--- network/src/main/java/com/desarrollodroide/network/model/LoginResponseMessageDTO.kt
+++ network/src/main/java/com/desarrollodroide/network/model/LoginResponseMessageDTO.kt
@@ -1,7 +1,15 @@
 package com.desarrollodroide.network.model
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginResponseMessageDTO (
-    val expires: Int?,    // Deprecated, used only for legacy APIs
-    val session: String?, // Deprecated, used only for legacy APIs
-    val token: String?,
-)
+    val expires: Int?, // Deprecated, used only for legacy APIs
+
+    // Map token to session
+    @SerializedName(value = "session", alternate = ["token"])
+    val session: String? // Deprecated, used only for legacy APIs
+) {
+    // Park token for now
+    val token: String? 
+        get() = session
+}
