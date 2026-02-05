811808d9df0715405055f1d715a62e9b0f515bda
appel <61596+appel@users.noreply.github.com>
2026-02-05 15:35:56 -0500

    Rollback

--- network/src/main/java/com/desarrollodroide/network/model/LoginResponseMessageDTO.kt
+++ network/src/main/java/com/desarrollodroide/network/model/LoginResponseMessageDTO.kt
@@ -1,14 +1,7 @@
 package com.desarrollodroide.network.model
 
-import com.google.gson.annotations.SerializedName
-
 data class LoginResponseMessageDTO (
-    val expires: Int?, // Deprecated, used only for legacy APIs
-
-    // Map token to session
-    @SerializedName(value = "session", alternate = ["token"])
+    val expires: Int?,    // Deprecated, used only for legacy APIs
     val session: String?, // Deprecated, used only for legacy APIs
-
-    @SerializedName("token")
-    val token: String? = session       
+    val token: String?,
 )
